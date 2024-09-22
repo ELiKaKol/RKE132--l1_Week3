8cb6bc9f71af25f1b4ce8eb4fdf0995df8babde2
EliKaKol <elisak.kolga@gmail.com>
2024-09-22 18:10:56 +0300

    add ulweek3 code

---
+++ Ül1ForWeek3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Enter your assessment");
+string userAssessment = Console.ReadLine();
+
+
+if (userAssessment == "A")
+{
+    Console.WriteLine("Suurepärane!");
+}
+else if (userAssessment == "B")
+{
+    Console.WriteLine("Väga hea!");
+}
+else if (userAssessment == "C")
+{
+    Console.WriteLine("Hea!");
+}
+else if (userAssessment == "D")
+{
+    Console.WriteLine("Rahuldav");
+}
+else if (userAssessment == "E")
+{
+    Console.WriteLine("Kasin");
+}
+else if (userAssessment == "F")
+{
+    Console.WriteLine("Puudulik");
+}
+else
+{
+    Console.WriteLine($"Vale väärtus");
+}
